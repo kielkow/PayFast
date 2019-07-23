f879e1fa80ff90e8722617cb36a89878c0baede0
kielkow <matheuskiel@fiorifer.com.br>
2019-07-22 21:39:28 -0300

    Add file pagamento.json / add path persistencia with database config/ include path persistencia in consign()

--- routes/routes.js
+++ routes/routes.js
@@ -7,4 +7,25 @@ module.exports = (app) => {
         )
     })
 
+    app.post('/pagamentos/pagamento', function(req, res){
+        
+        const pagamento = req.body
+        console.log('Processando req de pagamento')
+
+        pagamento.status = 'CRIADO'
+        pagamento.data = new Date
+        
+        const connection = app.persistencia.connectionFactory()
+        const pagamentoDao = new app.persistencia.PagamentoDao(connection)
+
+        pagamentoDao.salva(pagamento, function(erro, resultado){
+            if(erro){
+                console.log(erro)
+            }
+            console.log('Pagamento criado')
+            res.json(pagamento)
+        })
+
+    })
+
 }
