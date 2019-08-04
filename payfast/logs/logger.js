9e7e8d319adcba3a55190650d48f78d821f1f15c
kielkow <matheuskiel@fiorifer.com.br>
2019-08-04 16:17:26 -0300

    Add module winston to register logs

---
+++ payfast/logs/logger.js
@@ -0,0 +1,16 @@
+const winston = require('winston')
+
+const logger = new winston.createLogger({
+    transports: [
+        new winston.transports.File({
+            level: 'info',
+            filename: 'payfast.log',
+            maxsize: 100000,
+            maxFiles: 10,
+
+        })
+    ]
+})
+
+logger.log('info', 'Log com winston')
+logger.info('Log com info()')
